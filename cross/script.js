e0839a266995cf95f48186c9cc0826002b256fda
AsrorIbragimov <165019340+AsrorIbragimov@users.noreply.github.com>
2024-04-02 15:30:08 +0500

    Add files via upload

---
+++ cross/script.js
@@ -0,0 +1,42 @@
+
+
+
+//const user = "Yusuf"
+const user2 = "Yunus"
+/* console.log */
+console.log(`user first ${user}, user second ${user2}`);
+const user = 'Yusuf'
+//const user2 = 'Samar'
+
+// console.log('usur first' + ' ' + user + ' ' + 'user second' + ' ' + user2 );
+//console.log(user first ${user}, user second ${user2});
+
+
+const youtube = 'yusufKhamdamovvv'
+
+console.log(`https://youtube.com/@${youtube}`);
+
+// Операторы +, /, *, %, ++, --
+
+//let a = 5
+//let b = 7
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// console.log(a++);
+// console.log(b--);
+// console.log(a);
+// console.log(b);
+
+
+// && = и ,  = или
+
+console.log(b == 7  b == a); // true
+console.log(b == 7 && b == a); // false
+
+// console.log(7 === '7');
